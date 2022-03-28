de9096ce0ca7ed1c911c22bea33131f9d236c0b3
Smellyslab <ilike2code1234@gmail.com>
2022-03-28 15:40:31 +0000

    create files

---
+++ app.py
@@ -0,0 +1,12 @@
+import flask
+from flask import Flask 
+from flask import render_template 
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+app.run(host='0.0.0.0', port=8080, debug=True)
