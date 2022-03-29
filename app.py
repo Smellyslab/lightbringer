3deb9abb425059e54b6c6226145f5f7420a4b9e2
Smellyslab <ilike2code1234@gmail.com>
2022-03-29 14:49:46 +0000

    update site

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import flask
 from flask import Flask 
 from flask import render_template 
-
+from flask import send_file
+from flask import send_from_directory
 app = Flask(__name__)
 
 
@@ -9,4 +10,12 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/about', methods=['GET'])
+def about_page():
+    return render_template('about.html')
+
+@app.route('/purpose', methods=['GET'])
+def purpose_page():
+    return render_template('purpose.html')
+    
 app.run(host='0.0.0.0', port=8080, debug=True)
